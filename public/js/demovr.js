82321dc140f8346752863877e9772be07972315c
André Michel Andy Pozos <andreandyp@outlook.com>
2017-12-11 09:44:55 -0600

    Demo platanario

--- public/js/demovr.js
+++ public/js/demovr.js
@@ -1,4 +1,4 @@
-var maxX = 3, maxZ = 3;
+var maxX = Infinity, maxZ = Infinity;
 
 AFRAME.registerComponent('limite', {
     tick: function () {
@@ -11,34 +11,4 @@ AFRAME.registerComponent('limite', {
     }
   });
 $(document).ready(() =>{
-    var animacion = { elementos: []};
-    $("#cambiar").click(function(){
-        var conjunto = $("a-scene").children();
-
-        for(let elemento in conjunto){
-            var elem = { atributos: []};
-            var atributo = {};
-            
-            elem["etiqueta"] = conjunto[elemento].tagName.toLowerCase();
-
-            var atributos = conjunto[elemento].attributes;
-            for(let i = 0; i < atributos.length; i++){
-                if(atributos[i].value){
-                    atributo = {nombre: atributos[i].name, valor: atributos[i].value};
-                    elem["atributos"].push(atributo);
-                    atributo = {};
-                }                
-            }
-            animacion.elementos.push(elem);
-            animacion["maxX"] = maxX;
-            animacion["maxZ"] = maxZ;
-            if(conjunto[elemento].tagName.toLowerCase() === "a-camera"){
-                break;
-            }
-        }
-
-        var json = JSON.stringify(animacion);
-        console.log(json);
-        alert("Elemento guardado");
-    });
 });
